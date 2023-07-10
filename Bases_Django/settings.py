150fd17a5c2d4837d40623811578fb33c826748e
Ali Nando <suprenando12@gmail.com>
2023-07-10 10:34:22 +0200

    deployed with vercel

--- Bases_Django/settings.py
+++ Bases_Django/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-vji(%2by)2^c(q8aclfs-zg%_hjdg1$2q4c9txsjdofggzr3()
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =  True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
